beafdacb352ba14d206d709177e070ff3e847a2f
Omkar Pisal <okpisal007@gmail.com>
2026-01-21 16:10:17 +0530

    Constructor

---
+++ 38_ConstructorExampleDefault/38_ConstructorExampleDefault/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _38_ConstructorExampleDefault
+{
+    public class Employee
+    {
+        public Employee()
+        {
+            Console.WriteLine("Default Constructor");
+        }
+        
+        // color, name
+    }
+}
---
+++ 38_ConstructorExampleDefault/38_ConstructorExampleDefault/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _38_ConstructorExampleDefault
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // CTR is shortcut of Constructor
+
+            Employee employee = new Employee();
+            Console.ReadLine();
+        }
+    }
+}
