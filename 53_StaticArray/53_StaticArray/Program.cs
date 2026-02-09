521634991c88ddf0f708aacba47ee7a174b3cb2b
Omkar Pisal <okpisal007@gmail.com>
2026-02-09 23:57:07 +0530

    TryCatch

---
+++ 53_StaticArray/53_StaticArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _53_StaticArray
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Car = null;
+            if (Car != null)
+            {
+                foreach (string cars in Car)
+                    Console.WriteLine(cars);
+            }else
+            {
+                Console.WriteLine("NULL VALUE");
+            }
+        }
+    }
+}
---
+++ 54_ExceptionHandlingExample_TryCatch/54_ExceptionHandlingExample_TryCatch/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _54_ExceptionHandlingExample_TryCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string[] cars = { "Maruti", "Alto", "Wagnor", "Nano", "Swift" };
+                Console.WriteLine(cars[5]); // Index Out of Bound Exception
+            }
+            catch (IndexOutOfRangeException ex) // Specific Exception
+            {
+                Console.WriteLine(ex.Message.ToString());
+                Console.WriteLine(ex.StackTrace.ToString());
+                //Console.WriteLine(ex.InnerException.Message.ToString());
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+                Console.WriteLine(ex.StackTrace.ToString());
+                //Console.WriteLine(ex.InnerException.Message.ToString());
+            }
+            catch (Exception ex) // Generic exception
+            {
+                Console.WriteLine(ex.Message.ToString());
+                Console.WriteLine(ex.StackTrace.ToString());
+                //Console.WriteLine(ex.InnerException.Message.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("Execute");
+            }
+            
+
+            Console.Read();
+            }
+        }
+    }
